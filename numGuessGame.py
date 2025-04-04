65a3070d2682093022b11d45a717bd8e0b742d73
Andrew Dinh <andrewdinh74@yahoo.com>
2025-04-04 14:52:55 -0700

    Give user five tries to guess the number

--- numGuessGame.py
+++ numGuessGame.py
@@ -6,9 +6,17 @@ stop = int(args[2]) if len(args) >= 3 else 10
 
 numbers = list(range(start, stop + 1))
 target = random.choice(numbers)
-guess = int(input(f"Guess a number from {start} to {stop}, inclusive: "))
-if guess == target:
-  print("You are a genius!!")
-else:
-  print(f"Sorry, better lucky next time. The target was {target}.")
+guess = None
+tries = 5
+while tries > 0 and guess != target:
+  guess = int(input(f"Guess a number from {start} to {stop}, inclusive: "))
+  if guess == target:
+    print("You are a genius!!")
+    break
+  elif tries == 1:
+    print("Sorry, that's not it.  You are out of tries!")
+    break
+  else:
+    print(f"Sorry, better luck on the next guess.")
+    tries -= 1
 
